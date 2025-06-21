8ae95fa7809857a7dfe6e53fdb09931cf14a5a84
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-06-21 22:15:22 +0200

    (feat): change script

--- .github/archive.ps1
+++ .github/archive.ps1
@@ -1,3 +1,3 @@
 $json = Get-Content "./pack.json" | ConvertFrom-Json
-$filename = "{0}-{1}.zip" -f $json.name, $json.version
+$filename = "{0}-{1}-{2}.zip" -f $json.name, $json.mc_version, $json.version
 git archive -o $filename --add-file "./LICENSE" HEAD:src
--- .github/archive.sh
+++ .github/archive.sh
@@ -1,5 +1,6 @@
 NAME="$(cat pack.json | jq -r .name)"
 VERSION="$(cat pack.json | jq -r .version)"
+MC_VERSION="$(cat pack.json | jq -r .mc_version)"
 
-FILENAME="${NAME}-${VERSION}.zip"
+FILENAME="${NAME}-${MC_VERSION}-${VERSION}.zip"
 git archive -o $FILENAME --add-file "./LICENSE" HEAD:src

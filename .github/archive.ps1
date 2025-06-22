6f449c8e2795d896d7b683ba3cd980553cd9894d
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2025-06-22 16:13:18 +0200

    (feat): update to 1.19.2

--- .github/archive.ps1
+++ .github/archive.ps1
@@ -1,3 +1,3 @@
-$json = Get-Content "./pack.json" | ConvertFrom-Json
-$filename = "{0}-{1}-{2}.zip" -f $json.name, $json.mc_version, $json.version
+$AppProps = convertfrom-stringdata (get-content ./pack.properties -raw)
+$filename = "{0}-{1}-{2}.zip" -f $AppProps.name, $AppProps.mc_version, $AppProps.version
 git archive -o $filename --add-file "./LICENSE" HEAD:src
--- .github/archive.sh
+++ .github/archive.sh
@@ -1,6 +1,4 @@
-NAME="$(cat pack.json | jq -r .name)"
-VERSION="$(cat pack.json | jq -r .version)"
-MC_VERSION="$(cat pack.json | jq -r .mc_version)"
-
-FILENAME="${NAME}-${MC_VERSION}-${VERSION}.zip"
+#!/bin/bash
+source ./pack.properties
+FILENAME="${name}-${mc_version}-${version}.zip"
 git archive -o $FILENAME --add-file "./LICENSE" HEAD:src
